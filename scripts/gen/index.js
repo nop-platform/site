9c89b5005bc37849602ff226db81a1524ca5fdbc
Ourai Lin <ourairyu@gmail.com>
2024-04-02 20:14:11 +0800

    chore(site-zh): change KnoSys site structure and generate Nop Entropy docs

--- scripts/gen/index.js
+++ scripts/gen/index.js
@@ -1,7 +1,64 @@
-const { execute } = require('../helper');
+const { resolve: resolvePath } = require('path');
+const { isPlainObject } = require('@ntks/toolbox');
+
+const { getConfig, readData, saveData, execute } = require('../helper');
+
+function resolveDocToc(docs, docData) {
+  return docs.map(({ title, uri, children }) => {
+    const resolved = {};
+
+    if (title) {
+      resolved.text = title;
+    }
+
+    const slug = `${uri.replace(/(?:\/)?(index)?\.md$/, '') || 'index'}`;
+
+    resolved.slug = slug;
+    docData[slug] = { title: title || '', slug };
+
+    if (children) {
+      resolved.items = resolveDocToc(children, docData);
+    }
+
+    return resolved;
+  });
+}
+
+function resolveRepoData(config) {
+  const cookbook = readData(resolvePath(__dirname, './cookbook.yml'));
+  const siteDataDir = resolvePath(__dirname, '../..', `${config.source}/_data`)
+  const docs = readData(`${siteDataDir}/knosys/doc/docs.yml`);
+
+  const docData = {};
+  const docRepo = { name: 'Nop Entropy 文档', collection: 'projects/nop-entropy/docs', toc: resolveDocToc(docs.structure, docData) };
+
+  saveData(`${siteDataDir}/local/repos.yml`, { cookbook, doc: docRepo });
+  saveData(`${siteDataDir}/knosys/docs.yml`, { items: docData });
+}
 
 module.exports = {
-  execute: (site = 'default') => {
-    return execute('generate', site);
+  execute: (site = 'default', sourceKey) => {
+    const siteConfig = getConfig(`site.${site}`);
+    const { data } = siteConfig;
+
+    let keys = [];
+
+    if (data) {
+      if (isPlainObject(data)) {
+        if (sourceKey) {
+          if (data[sourceKey]) {
+            keys.push(sourceKey);
+          }
+        } else {
+          keys = Object.keys(data);
+        }
+      }
+    }
+
+    keys.forEach(key => execute('generate', site, key));
+
+    if (site === 'default') {
+      setTimeout(() => resolveRepoData(siteConfig));
+    }
   },
 };
--- scripts/helper.js
+++ scripts/helper.js
@@ -3,4 +3,4 @@ const { readFileSync } = require('fs');
 
 const rootPath = resolvePath(__dirname, '..');
 
-module.exports = require(resolvePath(rootPath, JSON.parse(readFileSync(resolvePath(rootPath, '.knosysrc'), 'utf8').toString().trim()).$path));
+module.exports = require(resolvePath(rootPath, JSON.parse(readFileSync(resolvePath(rootPath, '.knosys/config.json'), 'utf8').toString().trim()).$path));
