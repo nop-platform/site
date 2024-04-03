fbaf74af26d32395dacf625d3b5b5ca104ff84d5
Ourai Lin <ourairyu@gmail.com>
2024-04-03 14:01:11 +0800

    feat(script): remove temporary files with npm script

---
+++ scripts/clean.js
@@ -0,0 +1,22 @@
+const { resolve: resolvePath } = require('path');
+const { getConfig, rm } = require('./helper');
+
+const rootPath = resolvePath(__dirname, '..');
+const patterns = [
+  '/.cache/',
+  '/.asset-cache/',
+  '/.sass-cache/',
+  '/_assets/*/knosys/',
+  '/_data/knosys/',
+  '/_knosys/',
+];
+
+module.exports = {
+  execute: (site = 'default') => {
+    const { source } = getConfig(`site.${site}`);
+
+    if (source) {
+      patterns.forEach(p => rm(resolvePath(rootPath, `${source}/${p}`)));
+    }
+  },
+};
